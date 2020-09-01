5c6313fbd10a122bf5e5c12b29cca806dc179c89
Maria Rubinger <rubinger.maria@gmail.com>
2020-09-01 19:31:59 -0300

    Criação de gráfico de todos os países

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 import styles from './Cards.module.css';
 
@@ -10,9 +11,9 @@ const Cards = ({  data: { confirmed, recovered, deaths, lastUpdate  } }) => {
     }
 
     return (
-        <div classname={styles.container}>
+        <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
@@ -22,7 +23,7 @@ const Cards = ({  data: { confirmed, recovered, deaths, lastUpdate  } }) => {
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
@@ -32,7 +33,7 @@ const Cards = ({  data: { confirmed, recovered, deaths, lastUpdate  } }) => {
                         <Typography variant="body2">Number of recoveries from of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
